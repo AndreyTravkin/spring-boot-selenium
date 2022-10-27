d81f03db092bb640279cc084ec69037126396e81
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-10-27 07:42:02 -0500

    properties loader utils

--- src/test/java/com/spring/springselenium/SpringBaseTestNGTest.java
+++ src/test/java/com/spring/springselenium/SpringBaseTestNGTest.java
@@ -2,7 +2,9 @@ package com.spring.springselenium;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 public class SpringBaseTestNGTest extends AbstractTestNGSpringContextTests {
+
 }
