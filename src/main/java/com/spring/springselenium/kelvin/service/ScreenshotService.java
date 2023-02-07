bc1a70057e3171ef43774fe3fdadf31795010c90
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-02-07 07:28:43 -0600

    attach screenshots for failing tests

--- src/main/java/com/spring/springselenium/kelvin/service/ScreenshotService.java
+++ src/main/java/com/spring/springselenium/kelvin/service/ScreenshotService.java
@@ -40,4 +40,8 @@ public class ScreenshotService {
     File sourceFile = this.ctx.getBean(TakesScreenshot.class).getScreenshotAs(OutputType.FILE);
         FileCopyUtils.copy(sourceFile, this.path.resolve(faker.name().firstName() + ".png").toFile());
     }
+
+    public byte[] getScreenshot(){
+        return this.ctx.getBean(TakesScreenshot.class).getScreenshotAs(OutputType.BYTES);
+    }
 }
